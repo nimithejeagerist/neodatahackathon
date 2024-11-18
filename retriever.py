b7cfb3fd8a7d1509b2594d055d83e71e58ba582b
Nimi <takinroy@ualberta.ca>
2024-11-17 21:07:28 -0700

    wdweew

--- retriever.py
+++ retriever.py
@@ -9,7 +9,7 @@ import ssl
 
 
 # Neo4j Credentials
-NEO4J_URI =st.secrets(["neo4j"]["uri"])
+NEO4J_URI = st.secrets(["neo4j"]["uri"])
 NEO4J_USER = st.secrets(["neo4j"]["user"])
 NEO4J_PASSWORD = st.secrets(["neo4j"]["password"])
 
